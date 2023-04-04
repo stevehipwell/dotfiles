f852bc41083f0e508e2c2bda4289261c99e10497
Steve Hipwell <steve.hipwell@lexisnexisrisk.com>
2023-04-04 16:55:05 +0100

    Fixed scoop

--- .chezmoiscripts/run_once_before_000-configure-scoop.ps1
+++ .chezmoiscripts/run_once_before_000-configure-scoop.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 scoop alias add upgrade 'scoop update *' 'Updates all apps, just like brew or apt'
 
-scoop bucket add artifacthub
+scoop bucket add artifacthub https://github.com/artifacthub/scoop-cmd
 scoop bucket add extras
 scoop bucket add java
 scoop bucket add nerd-fonts
