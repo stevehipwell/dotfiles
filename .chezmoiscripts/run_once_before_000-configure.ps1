99e9568582264fabb1fbf87e533d32e94e68c1f6
Steve Hipwell <steve.hipwell@lexisnexisrisk.com>
2023-04-04 16:32:35 +0100

    Fixed scripts

--- .chezmoiscripts/run_once_before_000-configure.ps1
+++ .chezmoiscripts/run_once_before_000-configure.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 scoop alias add upgrade 'scoop update *' 'Updates all apps, just like brew or apt'
 
-scoop add bucket artifacthub
-scoop add bucket extras
-scoop add bucket java
-scoop add bucket nerd-fonts
+scoop bucket add artifacthub
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add nerd-fonts
--- .chezmoiscripts/run_onchange_before_001-install-powershell-modules.ps1
+++ .chezmoiscripts/run_onchange_before_001-install-powershell-modules.ps1
@@ -1,4 +1,6 @@
 $ErrorActionPreference = "Stop"
 
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 Install-Module -Name posh-git -RequiredVersion 1.1.0 -Scope CurrentUser
 Install-Module -Name PSReadLine -RequiredVersion 2.2.6 -Scope CurrentUser
