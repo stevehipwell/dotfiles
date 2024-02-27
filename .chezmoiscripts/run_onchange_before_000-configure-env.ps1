ea72d69cc282f07ee7aee0560cdf7ab75f5b0762
Steve Hipwell <steve.hipwell@gmail.com>
2024-02-27 15:24:37 +0000

    Updated
    Signed-off-by: Steve Hipwell <steve.hipwell@gmail.com>

--- .chezmoiscripts/run_onchange_before_000-configure-env.ps1
+++ .chezmoiscripts/run_onchange_before_000-configure-env.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-[Environment]::SetEnvironmentVariable('AWS_DEFAULT_REGION', '_', 'User')
+[Environment]::SetEnvironmentVariable('AWS_REGION', '_', 'User')
 [Environment]::SetEnvironmentVariable('AWS_PROFILE', '_', 'User')
 [Environment]::SetEnvironmentVariable('COMPOSE_DOCKER_CLI_BUILD', '1', 'User')
 [Environment]::SetEnvironmentVariable('DOCKER_BUILDKIT', '1', 'User')
@@ -9,4 +9,4 @@ $ErrorActionPreference = "Stop"
 [Environment]::SetEnvironmentVariable('SAML2AWS_USERNAME', '_', 'User')
 [Environment]::SetEnvironmentVariable('VAULT_ADDR', '_', 'User')
 [Environment]::SetEnvironmentVariable('VAULT_TOKEN', '_', 'User')
-[Environment]::SetEnvironmentVariable('WSLENV', 'AWS_DEFAULT_REGION/u:AWS_PROFILE/u:GHCR_TOKEN/u:SAML2AWS_USERNAME/u:VAULT_ADDR/u:VAULT_TOKEN/u', 'User')
+[Environment]::SetEnvironmentVariable('WSLENV', 'AWS_REGION/u:AWS_PROFILE/u:GHCR_TOKEN/u:SAML2AWS_USERNAME/u:VAULT_ADDR/u:VAULT_TOKEN/u', 'User')
